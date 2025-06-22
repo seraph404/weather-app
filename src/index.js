e80da1cceed2e4c5b20605f5ed031a8abb2698cd
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-06-22 16:13:02 -0400

    Create fetch request for weather API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./styles.css";
 
-const apiKey = process.env.WEATHER_API_KEY;
+const weatherButton = document.querySelector("#generate-weather");
+weatherButton.addEventListener("click", getWeatherData);
+
+function getWeatherData(event) {
+  event.preventDefault();
+  const apiKey = process.env.WEATHER_API_KEY;
+  const cityName = document.querySelector("#city");
+  const city = cityName.value;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`;
+  console.log(city);
+
+  if (!city) {
+    alert("Please enter a city name.");
+    return;
+  }
+
+  fetch(url, {
+    mode: "cors",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
